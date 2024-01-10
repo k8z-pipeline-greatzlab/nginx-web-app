2fabfb803ab061fe28bea1cc4cd792e2364fcc31
Greatzlab <90839886+theMartianLabs@users.noreply.github.com>
2024-01-09 20:11:33 -0400

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -60,3 +60,76 @@ resource "aws_lb_listener" "this" {
   }
   depends_on = [aws_instance.public_ec3]
 }
+
+###########
+# EC2 Instance
+###########
+resource "aws_instance" "public_ec3" {
+  for_each      = var.instance_name
+  ami           = each.value["image"]
+  instance_type = each.value["type"]
+  subnet_id     = data.aws_subnets.available.ids[0]
+  key_name      = var.key_name 
+  iam_instance_profile = var.iam_instance_profile
+
+  #user_data_base64 = base64encode(data.template_file.asg_user_data.rendered)
+
+  security_groups = [ "${aws_security_group.public_ec3_sg[0].id}" ]
+  lifecycle {
+    ignore_changes = [
+      security_groups
+    ]
+  }
+
+  tags = merge(var.tags,
+    {
+      Name = "${each.key}"
+  })
+}
+
+
+###########
+# Security Group
+###########
+resource "aws_security_group" "public_ec3_sg" {
+  count = length(var.instance_name) == 0 ? 0 : 1
+  
+  name        = "gitops_ansilble_sg"
+  description = "GitOps LAB sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "gitops_ansilble_sg"
+  }
+}
